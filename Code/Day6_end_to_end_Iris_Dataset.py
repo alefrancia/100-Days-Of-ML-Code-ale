fa4bfb8f260a7083d7fa73da643ea8794add96b7
Ale Francia <alefrancia@gmail.com>
2020-06-14 11:15:41 -0300

    Day 6

---
+++ Code/Day6_end_to_end_Iris_Dataset.py
@@ -0,0 +1,24 @@
+# This is an End to end project from https://machinelearningmastery.com/machine-learning-in-python-step-by-step/
+# by Jason Brownlee on February 10, 2019 in Python Machine Learning
+
+# Check the versions of libraries
+
+# Python version
+import sys
+print('Python: {}'.format(sys.version))
+# scipy
+import scipy
+print('scipy: {}'.format(scipy.__version__))
+# numpy
+import numpy
+print('numpy: {}'.format(numpy.__version__))
+# matplotlib
+import matplotlib
+print('matplotlib: {}'.format(matplotlib.__version__))
+# pandas
+import pandas
+print('pandas: {}'.format(pandas.__version__))
+# scikit-learn
+import sklearn
+print('sklearn: {}'.format(sklearn.__version__))
+
